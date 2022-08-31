b806a5d8b6f558f8421b2595f246c04303452ed6
darkcat7126 <darkcat7126@gmail.com>
2022-09-01 02:30:23 +0800

    PlayerAccount scriptable object

--- Assets/Darkcat/Script/ScriptableObject/PlayerAccount.cs
+++ Assets/Darkcat/Script/ScriptableObject/PlayerAccount.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "NewPlayer", menuName = "NewPlayer")]
 public class PlayerAccount : ScriptableObject
 {
     public int Player_Money;
